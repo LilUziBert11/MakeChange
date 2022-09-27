96b5711fbac4d10164359539f82a224c3cf4ef1c
albert <albert.duenas22@gmail.com>
2022-09-26 18:44:52 -0700

    Added a simple input validation. Need to check for case where user has 3 decimal places

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -1,4 +1,8 @@
 public class MakeChange {
+    private static boolean inputValidation(float userInput){
+        return userInput > 0;
+
+    }
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
